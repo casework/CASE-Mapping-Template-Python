73a1888ce2ba25e951a5e187fe16fa218d3c4df0
kchason <kchason@users.noreply.github.com>
2022-07-11 15:23:36 -0400

    Simplify generation

--- case_mapping/dict_to_case.py
+++ case_mapping/dict_to_case.py
@@ -1,11 +1,7 @@
-import hashlib
 import json
 import sys
-import uuid
-from datetime import datetime
-from os import path
 
-from .utilities import get_files_in_dir, hash_file, is_valid_path
+from .utilities import is_valid_path
 
 
 def main():
@@ -35,139 +31,34 @@ def main():
     }
 
     # Process the command line arguments to get the input and output paths
-    if len(sys.argv) > 2:
-        input_path = sys.argv[1]
-        output_path = sys.argv[2]
+    if len(sys.argv) > 1:
+        output_path = sys.argv[1]
 
-        # TODO check if paths are valid
-        if is_valid_path(input_path) and is_valid_path(output_path):
-            try:
-                files = get_files_in_dir(input_path)
+        # Check if paths are valid
+        if is_valid_path(output_path):
 
-                for file in files:
-                    # For each file, add it to the CASE graph
-                    case["@graph"].append(
-                        get_observable_file(path.join(input_path, file))
-                    )
+            # Add an uco-observable:ObservableObject to the graph
+            case["@graph"].append(
+                {
+                    "@id": "kb:organization-2b3b98e2-aea2-4270-876a-7f9917623cb6",
+                    "@type": "uco-identity:Organization",
+                    "uco-core:name": "Cyber Domain Ontology",
+                }
+            )
 
-                # Write the CASE graph to a file
-                with open(output_path, "w") as case_file:
-                    json.dump(case, case_file, ensure_ascii=False, indent=4)
+            # Write the CASE graph to a file
+            with open(output_path, "w") as case_file:
+                json.dump(case, case_file, ensure_ascii=False, indent=4)
 
-                # TODO print output path
-                sys.exit(0)
-            except FileNotFoundError as ex:
-                print(f"The file path is not valid: {ex}")
-                sys.exit(1)
+            # TODO print output path
+            sys.exit(0)
+
         else:
-            print("The input or output path is not valid")
+            print("The output path is not valid")
             sys.exit(1)
     else:
         print(f"Insufficient arguments. Usage is {sys.argv[0]} input_path output_path")
         sys.exit(1)
-
-
-def get_observable_file(filepath: str) -> dict:
-    """
-    Returns the file specified in the provided filepath as an ObservableObject in the CASE export. This method handles
-    calculation of filesize, extension, MD5 hash, SHA1 hash, and other metadata expected in the Observable TTL spec.
-
-    Ontology source: https://github.com/ucoProject/UCO/blob/master/uco-observable/observable.ttl.
-    """
-    if path.exists(filepath) and path.isfile(filepath):
-        # Get the full path we need for reference
-        filepath = path.abspath(filepath)
-
-        # Since the extension may take some additional logic checks, compute it out of the main JSON block
-        extension = path.splitext(filepath)[1]
-        if len(extension) > 0:
-            extension = extension[1:]
-
-        # Parse the file and get the attributes we need
-        return {
-            "@id": ("kb:" + str(uuid.uuid4())),
-            "@type": "uco-observable:File",
-            "uco-observable:hasChanged": False,
-            "uco-core:hasFacet": [
-                {
-                    "@type": "uco-observable:FileFacet",
-                    "uco-observable:observableCreatedTime": {
-                        "@type": "xsd:dateTime",
-                        "@value": datetime.fromtimestamp(
-                            path.getctime(filepath)
-                        ).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-                    },
-                    "uco-observable:modifiedTime": {
-                        "@type": "xsd:dateTime",
-                        "@value": datetime.fromtimestamp(
-                            path.getmtime(filepath)
-                        ).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-                    },
-                    "uco-observable:accessedTime": {
-                        "@type": "xsd:dateTime",
-                        "@value": datetime.fromtimestamp(
-                            path.getatime(filepath)
-                        ).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-                    },
-                    "uco-observable:extension": extension,
-                    "uco-observable:fileName": path.basename(filepath),
-                    "uco-observable:filePath": filepath,
-                    "uco-observable:isDirectory": False,
-                    "uco-observable:sizeInBytes": path.getsize(filepath),
-                },
-                {
-                    "@type": "uco-observable:ContentDataFacet",
-                    "uco-observable:hash": [
-                        {
-                            "@type": "uco-types:Hash",
-                            "uco-types:hashMethod": {
-                                "@type": "uco-vocabulary:HashNameVocab",
-                                "@value": "MD5",
-                            },
-                            "uco-types:hashValue": {
-                                "@type": "xsd:hexBinary",
-                                "@value": hash_file(filepath, hashlib.md5()),
-                            },
-                        },
-                        {
-                            "@type": "uco-types:Hash",
-                            "uco-types:hashMethod": {
-                                "@type": "uco-vocabulary:HashNameVocab",
-                                "@value": "SHA1",
-                            },
-                            "uco-types:hashValue": {
-                                "@type": "xsd:hexBinary",
-                                "@value": hash_file(filepath, hashlib.sha1()),
-                            },
-                        },
-                        {
-                            "@type": "uco-types:Hash",
-                            "uco-types:hashMethod": {
-                                "@type": "uco-vocabulary:HashNameVocab",
-                                "@value": "SHA256",
-                            },
-                            "uco-types:hashValue": {
-                                "@type": "xsd:hexBinary",
-                                "@value": hash_file(filepath, hashlib.sha256()),
-                            },
-                        },
-                        {
-                            "@type": "uco-types:Hash",
-                            "uco-types:hashMethod": {
-                                "@type": "uco-vocabulary:HashNameVocab",
-                                "@value": "SHA512",
-                            },
-                            "uco-types:hashValue": {
-                                "@type": "xsd:hexBinary",
-                                "@value": hash_file(filepath, hashlib.sha512()),
-                            },
-                        },
-                    ],
-                },
-            ],
-        }
-    else:
-        raise FileNotFoundError(f"File not found {filepath}")
 
 
 if __name__ == "__main__":
