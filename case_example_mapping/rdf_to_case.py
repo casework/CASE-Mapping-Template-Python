e8364841c0891e64343193afcb36a0f4f483679f
kchason <kchason@users.noreply.github.com>
2022-07-13 10:42:24 -0400

    Remove pre-write checks

--- case_example_mapping/rdf_to_case.py
+++ case_example_mapping/rdf_to_case.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 from rdflib import Graph, Literal, Namespace, URIRef
@@ -10,40 +9,37 @@ def main():
     if len(sys.argv) > 1:
         output_path: str = sys.argv[1]
 
-        # Check if paths are valid
-        if not os.path.isdir(output_path) and os.path.exists(
-            os.path.dirname(output_path)
-        ):
-            g: Graph = Graph()
-            g.bind("uco-core", "https://ontology.unifiedcyberontology.org/uco/core/")
-            g.bind(
-                "uco-identity",
-                "https://ontology.unifiedcyberontology.org/uco/identity/",
-            )
+        g: Graph = Graph()
+        g.bind("uco-core", "https://ontology.unifiedcyberontology.org/uco/core/")
+        g.bind(
+            "uco-identity",
+            "https://ontology.unifiedcyberontology.org/uco/identity/",
+        )
 
-            ns_core: Namespace = Namespace(
-                "https://ontology.unifiedcyberontology.org/uco/core/"
-            )
+        ns_core: Namespace = Namespace(
+            "https://ontology.unifiedcyberontology.org/uco/core/"
+        )
 
-            g.add(
-                (
-                    URIRef(
-                        "https://ontology.unifiedcyberontology.org/uco/identity/Organization"
-                    ),
-                    ns_core.name,
-                    Literal("Cyber Domain Ontology", datatype=XSD.string),
-                )
+        g.add(
+            (
+                URIRef(
+                    "https://ontology.unifiedcyberontology.org/uco/identity/Organization"
+                ),
+                ns_core.name,
+                Literal("Cyber Domain Ontology", datatype=XSD.string),
             )
+        )
 
-            # Write the CASE graph to a file
+        # Write the CASE graph to a file
+        try:
             with open(output_path, "w") as case_file:
                 case_file.write(g.serialize(format="json-ld", indent=4))
                 print(f"CASE graph exported to: {output_path}")
-
-            sys.exit(0)
-        else:
-            print(f"The output path is not valid: {output_path}")
+        except IOError:
+            print(f"Error writing to path: {output_path}")
             sys.exit(1)
+
+        sys.exit(0)
     else:
         print(f"Insufficient arguments. Usage is {sys.argv[0]} output_path")
         sys.exit(1)
