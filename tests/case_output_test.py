20459ee11722b7adc58adde228135e03007d3166
Alex Nelson <alexander.nelson@nist.gov>
2022-07-31 16:07:32 -0400

    Add test to confirm expected pair of statements in example output
    This test is known to fail CI currently, on observation of a code issue
    that will be corrected in the next patch.
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- tests/case_output_test.py
+++ tests/case_output_test.py
@@ -1,7 +1,8 @@
 import os
 import unittest
+from typing import Set
 
-from rdflib import Graph
+from rdflib import Graph, URIRef
 
 
 class CASEOutputTests(unittest.TestCase):
@@ -35,3 +36,43 @@ class CASEOutputTests(unittest.TestCase):
                 self.assertGreaterEqual(
                     len(g), 1, f"There were no triples found in file: {file}"
                 )
+
+    def test_organization_iri_found(self) -> None:
+        """
+        Identifies all CASE graph files within the ./output directory and ensures they contain a graph individual that:
+
+        * Is, by RDF class-membership, a UCO organization; and
+        * Has the UCO name "Cyber Domain Ontology".
+
+        When adapting this template repository, this test should be replaced.
+        """
+        files_tested: int = 0
+        source_directory: str = "./output"
+        query = """\
+PREFIX uco-core: <https://ontology.unifiedcyberontology.org/uco/core/>
+PREFIX uco-identity: <https://ontology.unifiedcyberontology.org/uco/identity/>
+SELECT ?nOrganization
+WHERE {
+  ?nOrganization
+    a uco-identity:Organization ;
+    uco-core:name "Cyber Domain Ontology" ;
+    .
+}
+"""
+        for file in os.listdir(source_directory):
+            if not (file.endswith(".json") or file.endswith(".jsonld")):
+                continue
+            files_tested += 1
+
+            computed: Set[URIRef] = set()
+
+            g: Graph = Graph()
+            g.parse(os.path.join(source_directory, file))
+            for result in g.query(query):
+                # Graph.query for a SELECT query returns a tuple, with
+                # member count as long as the number of bound variables.
+                computed.add(result[0])
+            self.assertEqual(
+                1, len(computed), f"Organization was not found in file: {file}"
+            )
+        self.assertGreaterEqual(files_tested, 0, "No output files were tested.")
